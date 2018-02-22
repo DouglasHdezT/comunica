6deaeedc8c90612939a91554316e92377c8173ce
CarlosDubon <carlosminerodubon@gmail.com>
2018-02-22 16:37:49 -0600

    nel

--- index.php
+++ index.php
@@ -155,7 +155,6 @@ if ( have_posts() ) {
         <h2>Multimedia</h2>
         <div class="row">
             <?php
-            wp_reset_postdata();
             if ( have_posts() ) {
                 while ( have_posts() ) {
                 the_post(); ?>
