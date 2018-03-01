4c40ad4b06c96a0feb0eefb314e6637e87895e1a
CarlosDubon <00058016@uca.edu.sv>
2018-03-01 12:01:41 -0600

    ahora

--- functions.php
+++ functions.php
@@ -56,17 +56,6 @@ function get_post_views($post_ID){
     return $count;
 }
 // Funcion para agregar widgets
-function comunica_widget(){
-    register_sidebar(array(
-        'name'=>'Radio',
-        'id'=>'idRadio'
-        'before_widget'=>'<div class="col-md-6">',
-        'after_widget'=>'</div>',
-        'before_title'=>'',
-        'after_title'=>''
-    ));
-}
-add_action( 'widgets_init', 'comunica_widget' );
 
 function getImage($num) {
     global $more;
