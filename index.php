e6dc9853984ba7d60abbe95a186c85e25524a2d8
CarlosDubon <00058016@uca.edu.sv>
2018-03-28 16:18:40 -0600

    ahora

--- index.php
+++ index.php
@@ -181,7 +181,7 @@ if ( have_posts() ) {
                                <div class="col-md-6" style="padding:0">
                                         <?php
                                         $youtube = getYTurl();
-                                        $full = 'https://www.youtube.com/embed/'.$youtube;
+                                        $full = 'https://www.youtube.com/embed/'.$youtube.'?autoplay=1';
                                         if(empty($youtube)){?>
                                             <div class="container-post">
                                                 <div class="img-media-container">
