2a1da3b4859a43ba423182b7012f2194f1b5042a
CarlosDubon <00058016@uca.edu.sv>
2018-04-09 14:00:52 -0600

    asi

--- index.php
+++ index.php
@@ -223,7 +223,7 @@ if ( have_posts() ) {
                     <?php dynamic_sidebar('podcast'); ?>
                 </div>
                 <div class="col-md-6">
-                   <div class="row" style="margin-bottom:15px">
+                   <div class="row" style="margin-bottom:15px;max-height:416px">
                        <?php dynamic_sidebar('sidebar-1'); ?>
                    </div>
                 </div>
