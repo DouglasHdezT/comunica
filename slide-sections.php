2f518386b02b6d2aba7d30ca6908832cab6eb4c9
CarlosDubon <00058016@uca.edu.sv>
2018-05-09 10:12:53 -0600

    fad

--- slide-sections.php
+++ slide-sections.php
@@ -10,31 +10,8 @@
                 while ( have_posts() ) {
                     the_post(); 
         ?>
-                        <div class="slide">
-                            <div class="col-sm-4">
-                                <div class="container-post">
-                                    <div class=" slider-container   material-container">
-                                        <div class="crop-image">
-                                            <?php
-                                              if ( has_post_thumbnail() ) {
-                                                the_post_thumbnail('post-thumbnails',array('class'=>'img-media'));
-                                              }
-                                            ?>
-                                        </div>
-                                    </div>
-                                    <div class="center-full-slide">
-                                        <div class="align-buttom-slide">
-                                            <h4 style="color:#fff">
-                                                <?php the_title() ?>
-                                            </h4>
-                                            <?php the_excerpt() ?>
-                                            <a href="<?php the_permalink() ?>" style="font-weight:bold;color:#fff;font-size:1em">Leer Más</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    <?php    
+                        
+        <?php    
                 } // end while
             } // end if
 
