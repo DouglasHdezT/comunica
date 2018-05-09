102c3868f82c874bba967dd4f16690c28db11fb2
CarlosDubon <00058016@uca.edu.sv>
2018-05-09 10:07:51 -0600

    slide

--- header.php
+++ header.php
@@ -36,39 +36,28 @@
     <div class="container-fluid">
         <div class="row">
         <?php
-            $theme_locations = get_nav_menu_locations();
-            $menu_obj = get_term( $theme_locations['secciones'], 'nav_menu' );
-            $menu_name = $menu_obj->name;
-            $items = wp_get_nav_menu_items($menu_name);
-            query_posts('');
-            for($i=0;$i<count($items);$i++){
-                        $flag = true;
-                        if ( have_posts() ) {
-                            while ( have_posts() ) {
-                                the_post();
-                                if(in_category($items[$i]->title)){
-                                    if($flag){
-                                    $url = nameToUrl($items[$i]->title);?>
-                                       <div class="last-new <?php $s = str_replace(' ','_',$items[$i]->title); echo $s ?>">
-                                           <div class="col-md-4">
-                                                <?php
-                                                    if ( has_post_thumbnail() ) {
-                                                    the_post_thumbnail('post-thumbnails',array('class'=>'img-responsive material-container'));
-                                                }?>
-                                            </div>
-                                            <div class="col-md-8">
-                                                <?php the_title(); ?>
-                                                <?php the_excerpt(); ?>
-                                                <a href="<?php the_permalink();?>" class="btn btn-secondary">Leer Más <span class="glyphicon glyphicon-chevron-right" style="font-weight: 100" aria-hidden="true"></span></a>
-                                                
-                                                <a href="<?php echo get_site_url().'/'.$url ?>" class="btn btn-secondary"><?php echo 'Ir a '.$items[$i]->title; ?> <span class="glyphicon glyphicon-chevron-right" style="font-weight: 100" aria-hidden="true"></span></a>
-                                                
-                                            </div>
-                                        </div>
-                                        <?php $flag = false; ?>
+ $theme_locations = get_nav_menu_locations(); $menu_obj = get_term( $theme_locations['secciones'], 'nav_menu' ); $menu_name = $menu_obj->name; $items = wp_get_nav_menu_items($menu_name); query_posts(''); for($i=0;$i
+<count($items);$i++){ $flag=t rue; if ( have_posts() ) { while ( have_posts() ) { the_post(); if(in_category($items[$i]->title)){ if($flag){ $url = nameToUrl($items[$i]->title);?>
+    <div class="last-new <?php $s = str_replace(' ','_',$items[$i]->title); echo $s ?>">
+        <div class="col-md-4">
+            <?php
+            if ( has_post_thumbnail() ) {
+            the_post_thumbnail('post-thumbnails',array('class'=>'img-responsive material-container'));
+            }?>
+        </div>
+        <div class="col-md-8">
+            <?php the_title(); ?>
+            <?php the_excerpt(); ?>
+            <a href="<?php the_permalink();?>" class="btn btn-secondary">Leer Más <span class="glyphicon glyphicon-chevron-right" style="font-weight: 100" aria-hidden="true"></span></a>
+
+            <a href="<?php echo get_site_url().'/'.$url ?>" class="btn btn-secondary"><?php echo 'Ir a '.$items[$i]->title; ?> <span class="glyphicon glyphicon-chevron-right" style="font-weight: 100" aria-hidden="true"></span></a>
+
+        </div>
+    </div>
+    <?php $flag = false; ?>
 
 
-                                <?php }
+    <?php }
                                 //
                                 }// Post Content here
                                 //
@@ -104,8 +93,8 @@
             'container_class'=>'menu-scrolling-bar',
             'items_wrap'=>'<ul class="menu-scrolling">%3$s</ul>'
 
-        ));
-        ?>
+            ));
+            ?>
         <div class="display-view padre">
             <div class="view hijo">
 
--- slide-sections.php
+++ slide-sections.php
@@ -7,7 +7,40 @@
         $items = wp_get_nav_menu_items($menu_name);
         query_posts('');
         foreach( $items as $item ) {
-            echo $item->title;
+            if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post();
+                    if(in_category($item->title)){?>
+                        <div class="slide">
+                            <div class="col-sm-4">
+                                <div class="container-post">
+                                    <div class=" slider-container   material-container">
+                                        <div class="crop-image">
+                                            <?php
+                                              if ( has_post_thumbnail() ) {
+                                                the_post_thumbnail('post-thumbnails',array('class'=>'img-media'));
+                                              }
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <div class="center-full-slide">
+                                        <div class="align-buttom-slide">
+                                            <h4 style="color:#fff">
+                                                <?php the_title() ?>
+                                            </h4>
+                                            <?php the_excerpt() ?>
+                                            <a href="<?php the_permalink() ?>" style="font-weight:bold;color:#fff;font-size:1em">Leer Más</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php    
+                    }
+
+                } // end while
+            } // end if
+
         }
 
         ?>
