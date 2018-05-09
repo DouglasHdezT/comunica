eedddfddea7233cac72386dcafcde7123bb3f394
CarlosDubon <00058016@uca.edu.sv>
2018-05-09 10:16:47 -0600

    dev

--- slide-sections.php
+++ slide-sections.php
@@ -5,18 +5,15 @@
         $menu_obj = get_term( $theme_locations['secciones'], 'nav_menu' );
         $menu_name = $menu_obj->name;
         $items = wp_get_nav_menu_items($menu_name);
+        wp_reset_query();
         foreach( $items as $item ) {
             if ( have_posts() ) {
                 while ( have_posts() ) {
                     the_post(); 
-        ?>
-                        
-        <?php    
+                    echo the_title();
                 } // end while
             } // end if
-
         }
-
         ?>
     </section>
 </div>
