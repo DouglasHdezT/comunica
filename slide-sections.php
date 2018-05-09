8168b45bbb6216aadb3ca206d42f3efb7f6679f2
CarlosDubon <00058016@uca.edu.sv>
2018-05-09 10:09:59 -0600

    asdf

--- slide-sections.php
+++ slide-sections.php
@@ -5,12 +5,11 @@
         $menu_obj = get_term( $theme_locations['secciones'], 'nav_menu' );
         $menu_name = $menu_obj->name;
         $items = wp_get_nav_menu_items($menu_name);
-        query_posts('');
         foreach( $items as $item ) {
             if ( have_posts() ) {
                 while ( have_posts() ) {
                     the_post();
-                    if(in_category($item->title)){?>
+                    if(in_category($item->title)){ ?>
                         <div class="slide">
                             <div class="col-sm-4">
                                 <div class="container-post">
