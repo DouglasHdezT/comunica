202e9e4cd391713e7a5d557ef3f44061a4a31ea7
00062816 <00062816@uca.edu.sv>
2018-05-10 20:23:35 -0600

    Test

--- slide-sections.php
+++ slide-sections.php
@@ -1,23 +1,16 @@
 <div class="no-slide">
     <section id="slideshow-sections">
         <?php
-        $theme_locations = get_nav_menu_locations();
-        $menu_obj = get_term( $theme_locations['secciones'], 'nav_menu' );
-        $menu_name = $menu_obj->name;
-        $count_secc = 1;
-        $open = 0;
-        $items = wp_get_nav_menu_items($menu_name);
-        foreach( $items as $item ) {
-
-            if ( have_posts() ) {
-              while ( have_posts() ) {
-                if($count_secc == 1 && $open == 0){
-                  echo '<div class="slide">';
-                  $open=1;
-                }
-                the_post();
-                  if(in_category($item->title)){
-                    ?>
+        $args = array(
+            'cat' => $category->term_id,
+            'post_type' => 'post',
+            'posts_per_page' => '1',
+        );
+        $query = new WP_Query( $args );
+        if ( $query->have_posts() ) {
+          while ( $query->have_posts() ) {
+            $query->the_post();
+        ?>
                     <div class="col-sm-3" style="padding:0">
                         <div class="container-post">
                             <div class=" slider-container   material-container">
@@ -35,27 +28,12 @@
                                         <?php the_title(); ?>
                                     </h4>
                                     <?php the_excerpt(); ?>
-                                    <a href='<?php the_permalink(); ?>' style="font-weight:bold;color:#fff;font-size:1em">Leer Más</a>
+                                    <a href="<?php the_permalink(); ?>" style="font-weight:bold;color:#fff;font-size:1em">Leer Más</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <?php
-                    if($count_secc == 4 && $open==1){
-                      echo '</div>';
-                      $count_secc =1;
-                      $open=0;
-                    }
-                    $count_secc++;
-                    }
-                  }
-        }
-
-      }
-      if($count_secc != 0){
-        echo '</div>';
-      }
-
-        ?>
+                  <?php }
+                }?>
     </section>
 </div>
