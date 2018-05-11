49b78478419a6ca22fbd7197707287220c7281cb
00062816 <00062816@uca.edu.sv>
2018-05-10 19:20:28 -0600

    sigh

--- slide-sections.php
+++ slide-sections.php
@@ -4,9 +4,10 @@
         $theme_locations = get_nav_menu_locations();
         $menu_obj = get_term( $theme_locations['secciones'], 'nav_menu' );
         $menu_name = $menu_obj->name;
-        $cont_secc = 4;
-        $open = 0;
+        $count_secc = 4;
+        $open = 1;
         $items = wp_get_nav_menu_items($menu_name); ?>
+        <div class="slide">
         <?php
         foreach( $items as $item ) {
             if ( have_posts() ) {
@@ -17,8 +18,9 @@
               }
               while ( have_posts() ) {
                 the_post();
+                  if(in_category($item->title) && has_post_thumbnail()){
                     ?>
-                    <div class="col-sm-3" style="padding:0" background="#FFF">
+                    <div class="col-sm-3" style="padding:0">
                         <div class="container-post">
                             <div class=" slider-container   material-container">
                                 <div class="crop-image-slider">
@@ -43,9 +45,10 @@
                     <?php
                     $count_secc--;
                     break;
+                    }
                   }
-                  if($count_secc==0 && $open=1){
-                    echo '</div>';
+                  if($count_secc==0 && $open==1){
+                    echo '</div >';
                     $open=0;
                   }
         }
