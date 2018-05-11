a6a95a02034c1ef94fbaac9dce3a4384b45d21c8
00062816 <00062816@uca.edu.sv>
2018-05-10 18:13:59 -0600

    Weird issue

--- slide-sections.php
+++ slide-sections.php
@@ -5,36 +5,40 @@
         $menu_obj = get_term( $theme_locations['secciones'], 'nav_menu' );
         $menu_name = $menu_obj->name;
         $cont_secc = 4;
+        $open = 0;
         $items = wp_get_nav_menu_items($menu_name);
         foreach( $items as $item ) {
             if ( have_posts() ) {
-              if($cont_secc==4){
-                echo '<div class="slide">';}
+              if($open==0){
+                echo '<div class="slide">';
+                $open==1;
+              }
                 while ( have_posts() ) {
                   the_post();
                   if(in_category($item->title) && has_post_thumbnail()){
                     ?>
-                            <div class="col-sm-3">
-                                <div class="container-post">
-
-                                    <div class=" slider-container   material-container">
-                                        <div class="crop-image">
-                                            <?php
-                                                the_post_thumbnail('post-thumbnails',array('class'=>'img-media'));
-                                            ?>
-                                        </div>
-                                    </div>
-                                    <div class="center-full-slide">
-                                        <div class="align-buttom-slide">
-                                            <h4 style="color:#fff">
-                                                <?php the_title() ?>
-                                            </h4>
-                                            <?php the_excerpt() ?>
-                                            <a href="<?php the_permalink() ?>" style="font-weight:bold;color:#fff;font-size:1em">Leer Más</a>
-                                        </div>
-                                    </div>
+                    <div class="col-sm-4" style="padding:0">
+                        <div class="container-post">
+                            <div class=" slider-container   material-container">
+                                <div class="crop-image-slider">
+                                    <?php
+                                      if ( has_post_thumbnail() ) {
+                                        the_post_thumbnail('post-thumbnails',array('class'=>'img-media'));
+                                      }
+                                    ?>
                                 </div>
                             </div>
+                            <div class="center-full-slide">
+                                <div class="align-buttom-slide">
+                                    <h4 style="color:#fff">
+                                        <?php the_title() ?>
+                                    </h4>
+                                    <?php the_excerpt() ?>
+                                    <a href="<?php the_permalink() ?>" style="font-weight:bold;color:#fff;font-size:1em">Leer Más</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <?php
                     $count_secc--;
                     break;
@@ -42,10 +46,12 @@
             }
             if($count_secc==0){
               echo '</div>';
-              $count_secc=4;}
+              $count_secc=4;
+              $open=0;
+            }
         }
       }
-      if($count_secc!=4){
+      if($open==1){
         echo '</div>';
       }
         ?>
