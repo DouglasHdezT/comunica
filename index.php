f354b4bae573beba5e4333b2491dea429776f8f8
CarlosDubon <00058016@uca.edu.sv>
2018-05-14 14:51:13 -0600

    cambio de color

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ if ( have_posts() ) {
             </div>
             <br>
 
-            <div class="row material-container" style="background:#072844;">
+            <div class="row material-container" style="background:#7f6ae8;">
                 <div class="col-md-12">
                     <h2 style="padding:10px; color:#fff;padding-top:25px">ÚLTIMAS ENTRADAS</h2><br>
                     <!-- SLIDE MOVIL -->
@@ -240,7 +240,7 @@ if ( have_posts() ) {
                 </div>
             </div>
             <!--Area Secciones-->
-            <div class="row" style="background:#7f6ae8">
+            <div class="row" style="background:#7f6ea8">
                 <div class="col-md-12 material-container">
                     <h2 style="color:#fff;padding-top:25px;padding-bottom:20px">Secciones</h2>
                     <?php
