48610cf2831e326b8afa1f5aa7704debe1f32015
CarlosDubon <00058016@uca.edu.sv>
2018-05-15 00:12:24 -0600

    maximo de tamaño

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+@ini_set( 'upload_max_size' , '15M' );
+@ini_set( 'post_max_size', '15M');
+@ini_set( 'max_execution_time', '300' );
+
 if(function_exists('register_nav_menus')){
     register_nav_menus(array(
         'secciones'=>'Secciones parte superior',
