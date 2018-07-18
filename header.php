215a6e264002cf954b80ae1c0e5a4d2d0f9f7e22
CarlosDubon <00058016@uca.edu.sv>
2018-07-18 11:11:57 -0600

    correccion de estilos

--- header.php
+++ header.php
@@ -27,6 +27,7 @@
                     the_post_thumbnail('post-thumbnails',array('class'=>'initial-banner'));
                 }
                 echo "</div>";
+                echo "<div class='filter-banner'></div>"
                 $flag = false;
             }
         } // end while
