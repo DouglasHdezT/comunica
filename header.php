46ffd18c1a4454f5ffdd2b43f72d95cf298333f5
CarlosDubon <00058016@uca.edu.sv>
2018-10-13 14:19:38 -0600

    fix nav

--- header.php
+++ header.php
@@ -71,7 +71,7 @@
         $menu_obj = get_term( $theme_locations['secciones'], 'nav_menu' );
         $menu_name = $menu_obj->name;
         $items = wp_get_nav_menu_items($menu_name);
-        echo $items;
+        print_r ($items);
         query_posts('');
         for($i=0;$i<count($items);$i++){
           $flag = true;
