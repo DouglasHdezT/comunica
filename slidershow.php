b4f22c57b872ea5a8f7afd01af1d80a19f670c13
DouglasHdezT <00004516@uca.edu.sv>
2018-10-27 12:30:02 -0600

    Slider main

--- slidershow.php
+++ slidershow.php
@@ -5,25 +5,30 @@
     if ( have_posts() ) {
       do {
         ?>
-        <div class="slide">
+        <div class="slide"> 
           <?php
           for($i=0;$i<=2;$i++){
             if(have_posts()){
-            the_post();?>
-            <div class="container-slide">
-              <a href="<?php the_permalink(); ?>">
-                <?php
-                if ( has_post_thumbnail() ) {
-                  the_post_thumbnail('post-thumbnails',array('class'=>'img-slide'));
-                }
-                ?>
-              </a>
-              <div class="display-slide">
-                <h3><?php the_title(); ?></h3>
-                <p><?php the_excerpt(); ?></p>
+              the_post();
+              echo "NEl";
+              ?>
+              <div class="container-slide">
+                <a href="<?php the_permalink(); ?>">
+                  <?php
+                  if ( has_post_thumbnail() ) {
+                    the_post_thumbnail('post-thumbnails',array('class'=>'img-slide'));
+                  }
+                  ?>
+                </a>
+                <div class="display-slide">
+                  <h3><?php the_title(); ?></h3>
+                  <p><?php the_excerpt(); ?></p>
+                </div>
               </div>
-            </div>
-          <?php}}?>
+          <?php
+            }
+          }
+          ?>
         </div>
       <?php $contSlider+=1;} while (have_posts());// end while
     } // end if
