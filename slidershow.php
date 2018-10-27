b49098cdd20bfc7dba07cd4d8394cf5b3d4f921a
DouglasHdezT <00004516@uca.edu.sv>
2018-10-27 12:09:49 -0600

    Slider main

--- slidershow.php
+++ slidershow.php
@@ -1,26 +1,34 @@
 <div class="no-slide">
   <section id="slideshow">
     <?php
+    $contSlider = 0;
     if ( have_posts() ) {
       do {
-        the_post();?>
+        the_post();
+        ?>
         <div class="slide">
-          <div class="container-slide">
-            <a href="<?php the_permalink(); ?>">
-              <?php
-              if ( has_post_thumbnail() ) {
-                the_post_thumbnail('post-thumbnails',array('class'=>'img-slide'));
-              }
-              ?>
-            </a>
-            <div class="display-slide">
-              <h3><?php the_title(); ?></h3>
-              <p><?php the_excerpt(); ?></p>
+          <?php
+          for($i=0;$i<=2;$i++){?>
+            <div class="container-slide">
+              <a href="<?php the_permalink(); ?>">
+                <?php
+                if ( has_post_thumbnail() ) {
+                  the_post_thumbnail('post-thumbnails',array('class'=>'img-slide'));
+                }
+                ?>
+              </a>
+              <div class="display-slide">
+                <h3><?php the_title(); ?></h3>
+                <p><?php the_excerpt(); ?></p>
+              </div>
             </div>
-          </div>
+          <?php}?>
         </div>
-      <?php } while ( have_posts() );// end while
+      <?php $contSlider++;} while (have_posts() && $contSlider <=5);// end while
     } // end if
     ?>
+    <a href="#" class="slidesjs-previous slidesjs-navigation"><span class="glyphicon glyphicon-menu-left navegation" aria-hidden="true" style="font-size:3em"></span>
+    </a>
+    <a href="#" class="slidesjs-next slidesjs-navigation"><span class="glyphicon glyphicon-menu-right navegation" aria-hidden="true" style="font-size:3em"></span></a>
   </section>
 </div>
