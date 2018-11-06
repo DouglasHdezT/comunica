3e036295c4bd8b3e7d607fb0e5b8b21a8e81f490
DouglasHdezT <00004516@uca.edu.sv>
2018-11-06 10:13:30 -0600

    SPC

--- slide-sections.php
+++ slide-sections.php
@@ -49,7 +49,7 @@
       }
     }
     ?>
-    <a href="#"  class="slidesjs-previous slidesjs-navigation"><span class="glyphicon glyphicon-menu-left navegation" aria-hidden="true" style="font-size:3em"></span></a>
-    <a href="#" class="slidesjs-next slidesjs-navigation"><span class="glyphicon glyphicon-menu-right navegation" aria-hidden="true" style="font-size:3em"></span></a>
   </section>
+  <a href="#"  class="slidesjs-previous slidesjs-navigation"><span class="glyphicon glyphicon-menu-left navegation" aria-hidden="true" style="font-size:3em"></span></a>
+    <a href="#" class="slidesjs-next slidesjs-navigation"><span class="glyphicon glyphicon-menu-right navegation" aria-hidden="true" style="font-size:3em"></span></a>
 </div>
