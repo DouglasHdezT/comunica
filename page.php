0141d289750c6b27bbb04f5dbc6434ee202b8dd0
DouglasHdezT <00004516@uca.edu.sv>
2018-11-06 15:47:40 -0600

    Page fix

--- page.php
+++ page.php
@@ -71,41 +71,36 @@
     <!-- Noticias relacionadas con la pagina -->
     <div class="row" style="margin-top:20px;">
       <?php
-      query_posts(''); //Reinicio de querys para obtener todos los post
-      if ( have_posts() ) {
-        while ( have_posts() ) {
-          the_post();
-          $post_views = get_post_views(get_the_ID());
-          $post_categories= get_the_category(get_the_ID());
-          foreach($post_categories as $post_category_obj){
-            $post_category = $post_category_obj->name;
-            if($post_category == $name_category && $post->ID != $idFirst){
+      $allPosts = get_posts(array(
+        'numberposts'=>-1,
+        'category'=>$name_category
+      )); //Reinicio de querys para obtener todos los post
+
+      foreach($allPosts as $post){
+        setup_postdata($post);
+        $post_id = get_the_ID();
+        if($post_id == $idFirst) continue;
+        ?>
+          <div class="col-md-4">
+            <div class="card mb-3  slider-container">
+              <h3 id="style-7" class="card-header title-page-post"><?php the_title();?></h3>
+              <?php
+              if ( has_post_thumbnail() ) {
+                the_post_thumbnail('post-thumbnails',array('class'=>'slider-hght'));
+              }
               ?>
-              <div class="col-md-4">
-                <div class="card mb-3  slider-container">
-                  <h3 id="style-7" class="card-header title-page-post"><?php the_title();?></h3>
-                  <?php
-                  if ( has_post_thumbnail() ) {
-                    the_post_thumbnail('post-thumbnails',array('class'=>'slider-hght'));
-                  }
-                  ?>
-                  <div class="card-body text-dark post-pages">
-                    <p class="card-text"><?php the_excerpt(); ?></p>
-                    <a href="<?php the_permalink();?>" class="btn btn-outline-primary">Leer más</a>
-                  </div>
-                  <div class="card-footer text-muted">
-                    <small class="text-muted"><?php echo get_the_date(); ?> - <?php the_time();?> / <?php the_author() ?></small>
-                  </div>
-                </div>
-
+              <div class="card-body text-dark post-pages">
+                <p class="card-text"><?php the_excerpt(); ?></p>
+                <a href="<?php the_permalink();?>" class="btn btn-outline-primary">Leer más</a>
               </div>
-
-            <?php }
-          }
-        }// end while
-      } // end if
-
-
+              <div class="card-footer text-muted">
+                <small class="text-muted"><?php echo get_the_date(); ?> - <?php the_time();?> / <?php the_author() ?></small>
+              </div>
+            </div>
+          </div>
+        <?php
+      }
+      
       wp_reset_query(); //Volver a la query original.
       ?>
     </div>
