6f8bb54ad0458cec08b62d165e55a6459b32e00e
DouglasHdezT <00004516@uca.edu.sv>
2018-11-05 18:40:03 -0600

    SPC

--- slide-sections.php
+++ slide-sections.php
@@ -2,9 +2,8 @@
   <section id="slideshow-sections">
     <?php
     $counter=1;
-    $flagged=0;
     $categories = get_categories();
-    foreach ( $categories as $category ) {
+    foreach($categories as $category){
       $nameUrl = $category->name;
       $url = nameToUrl($category->name);
       $args = array(
@@ -13,45 +12,44 @@
         'posts_per_page' => '1',
       );
       $query = new WP_Query( $args );
-      if ( $query->have_posts()) {
-        while ( $query->have_posts() ) {
+
+      if($query->have_posts()){
+        while($query->have_posts()){
           $query->the_post();
-          if($counter==1 && $flagged==0){
+          
+          if($counter==1){
             echo "<div class='slider'>";
-            $flagged=1;
-          }
-          ?>
+          }?>
+
           <div class="col-sm-3" style="padding:0">
-              <div class="container-post">
-                <div class="material-container">
-                    <div class="crop-image-slider-sections">
-                      <?php
-                      if ( has_post_thumbnail() ) {
-                        the_post_thumbnail('post-thumbnails',array('class'=>'img-media lazyload'));
-                      }
-                      ?>
+            <div class="container-post">
+              <div class="material-container">
+                  <div class="crop-image-slider-sections">
+                    <?php
+                    if ( has_post_thumbnail() ) {
+                      the_post_thumbnail('post-thumbnails',array('class'=>'img-media lazyload'));
+                    }
+                    ?>
+                  </div>
+                  <a class="a" href="<?php echo $siteurl.'/'.$url; ?>">
+                    <div class="center-full-slide">
+                      <span style="color:#fff;font-weight:bold;font-size:2rem; padding:0.3em; background:#00000088;"><?php echo $nameUrl; ?></span>
                     </div>
-                  
-                </div>
-                <a class="a" href="<?php echo $siteurl.'/'.$url; ?>">
-                <div class="center-full-slide">
-                  <span style="color:#fff;font-weight:bold;font-size:2rem; padding:0.3em; background:#00000088;"><?php echo $nameUrl; ?></span>
-                </div>
-                </a>
+                  </a>
               </div>
+            </div>
           </div>
           <?php
           $counter++;
-          if($counter>4 && $flagged==1){
+          if($counter>4){
             echo "</div>";
             $counter=1;
-            $flagged=0;
           }
         }
       }
     }
     ?>
     <a href="#"  class="slidesjs-previous slidesjs-navigation"><span class="glyphicon glyphicon-menu-left navegation" aria-hidden="true" style="font-size:3em"></span></a>
-  <a href="#" class="slidesjs-next slidesjs-navigation"><span class="glyphicon glyphicon-menu-right navegation" aria-hidden="true" style="font-size:3em"></span></a>
+    <a href="#" class="slidesjs-next slidesjs-navigation"><span class="glyphicon glyphicon-menu-right navegation" aria-hidden="true" style="font-size:3em"></span></a>
   </section>
 </div>
